be7a6f2089d6f55b0c3381466847c4032d509d7f
asapbox <tyumenpost@gmail.com>
2022-05-01 12:19:23 +0700

    Adding the text to the card1.dart

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 class Card1 extends StatelessWidget {
   const Card1({Key? key}) : super(key: key);
@@ -12,18 +13,48 @@ class Card1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline5,
+              ),
+              top: 20,
+            ),
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 10,
+            ),
+          ],
+        ),
         padding: const EdgeInsets.all(16),
         constraints: const BoxConstraints.expand(
           width: 350,
           height: 450,
         ),
         decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/mag1.png'),
-            fit: BoxFit.cover,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(10.0))
-        ),
+            image: DecorationImage(
+              image: AssetImage('assets/mag1.png'),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
       ),
     );
   }
